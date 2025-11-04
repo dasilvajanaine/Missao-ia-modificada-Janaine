e7f0abbd001b2a3f859c7d5b4562c054998b4f52
Janaina da Silva Rodrigues <145395931+dasilvajanaine@users.noreply.github.com>
2025-11-04 14:53:35 -0300

    04010

--- main.js
+++ main.js
@@ -11,11 +11,19 @@ const perguntas = [
         alternativas: [
             {
                 texto: " Raramente",
-                afirmacao: " Raramente a ansiedade te domina, você cultiva presença e gratidão para manter sua paz.",
+                afirmacao: [
+                    " Raramente a ansiedade te domina, você cultiva presença e gratidão para manter sua paz.",
+                    "AFIRMACAO 2",
+                    "AFIRMACAO 3"
+                ]
             },
             {
                 texto: " Frequentemente ",
-                afirmacao: " Quando a ansiedade chega, você respira, aceita o que sente e lembra que tem recursos para  se acalmar.",
+                afirmacao: [
+                    " Quando a ansiedade chega, você respira, aceita o que sente e lembra que tem recursos para  se acalmar.",
+                    "AFIRMACAO 2",
+                    "AFIRMACAO 3"
+                ]
             }
         ]
     },
@@ -24,11 +32,19 @@ const perguntas = [
         alternativas: [
             {
                 texto: " Eu foco na respiração, fazendo-a de forma lenta e controlada, e também escrevo sobre meus sentimentos para processá-los.",
-                afirmacao: " Essa abordagem demonstra uma maneira eficaz de lidar com a ansiedade, pois combina técnicas de relaxamento físico, como a respiração controlada, com a expressão emocional, por meio da escrita.",
+                afirmacao: [
+                    " Essa abordagem demonstra uma maneira eficaz de lidar com a ansiedade, pois combina técnicas de relaxamento físico, como a respiração controlada, com a expressão emocional, por meio da escrita.",
+                    "AFIRMACAO 2",
+                    "AFIRMACAO 3"
+                ]
             },
             {
                 texto: "  Eu saio para caminhar e tento me concentrar no momento presente, usando os sentidos para me acalmar, além de recorrer a um aplicativo de meditação para relaxar rapidamente.",
-                afirmacao: " Caminhar com atenção e meditar trazem calma e equilíbrio imediato.",
+                afirmacao: [
+                    " Caminhar com atenção e meditar trazem calma e equilíbrio imediato.",
+                    "AFIRMACAO 2",
+                    "AFIRMACAO 3"
+                ]
             }
         ]
     },
@@ -37,11 +53,19 @@ const perguntas = [
         alternativas: [
             {
                 texto: " Não, os sintomas da ansiedade são apenas emocionais.",
-                afirmacao: " Palpitações, sudorese, tontura e tensão muscular são manifestações físicas comuns da ansiedade, resultantes da ativação do sistema nervoso.",
+                afirmacao: [
+                    " Palpitações, sudorese, tontura e tensão muscular são manifestações físicas comuns da ansiedade, resultantes da ativação do sistema nervoso.",
+                    "AFIRMACAO 2",
+                    "AFIRMACAO 3"
+                ]
             },
             {
                 texto: " Sim, o corpo reage à ansiedade como se estivesse em perigo real.",
-                afirmacao: " O corpo reage à ansiedade como se estivesse em perigo real para enfrentar ou fugir de uma ameaça, o que explica os sintomas físicos como batimentos cardíacos acelerados, respiração ofegante e tensão muscular.",
+                afirmacao: [
+                    " O corpo reage à ansiedade como se estivesse em perigo real para enfrentar ou fugir de uma ameaça, o que explica os sintomas físicos como batimentos cardíacos acelerados, respiração ofegante e tensão muscular.",
+                    "AFIRMACAO 2",
+                    "AFIRMACAO 3"
+                ]
             }
         ]
     },
@@ -50,11 +74,19 @@ const perguntas = [
         alternativas: [
             {
                 texto: " Sim",
-                afirmacao: " Quando a ansiedade interferir no seu trabalho ou estudos, busque estratégias para retomar o equilíbrio.",
+                afirmacao: [
+                    " Quando a ansiedade interferir no seu trabalho ou estudos, busque estratégias para retomar o equilíbrio.",
+                    "AFIRMACAO 2",
+                    "AFIRMACAO 3"
+                ]
             },
             {
                 texto: " Não",
-                afirmacao: " Como a ansiedade não atrapalha nas suas atividades, aproveite esse estado de tranquilidade para manter meu foco e produtividade.",
+                afirmacao: [
+                    "Como a ansiedade não atrapalha nas suas atividades, aproveite esse estado de tranquilidade para manter meu foco e produtividade.",
+                    "AFIRMACAO 2",
+                    "AFIRMACAO 3"
+                ]
             }
         ]
     },
@@ -63,11 +95,21 @@ const perguntas = [
         alternativas: [
             {
                 texto: " Não, elas são ineficazes para sintomas de ansiedade.",
-                afirmacao: "A afirmação de que meditação e exercícios de respiração são ineficazes é falsa. Na verdade, essas práticas são ferramentas valiosas e acessíveis para o gerenciamento da ansiedade.",
+                afirmacao: [
+                    "A afirmação de que meditação e exercícios de respiração são ineficazes é falsa. Na verdade, essas práticas são ferramentas valiosas e acessíveis para o gerenciamento da ansiedade.",
+                    "AFIRMACAO 2",
+                    "AFIRMACAO 3"
+                ]
             },
             {
                 texto: " Sim, elas ajudam a acalmar o sistema nervoso e a focar a mente.",
-                afirmacao: " Ao focar a atenção na respiração e no momento presente, essas práticas ajudam a reduzir a frequência cardíaca, a pressão arterial e a liberar a tensão muscular, combatendo os sintomas físicos da ansiedade.",
+                afirmacao: [
+                    " Ao focar a atenção na respiração e no momento presente, essas práticas ajudam a reduzir a frequência cardíaca, a pressão arterial e a liberar a tensão muscular, combatendo os sintomas físicos da ansiedade.",
+                    "AFIRMACAO 2"
+                     "AFIRMACAO 3"
+
+                ]
+
             }
         ]
     },
